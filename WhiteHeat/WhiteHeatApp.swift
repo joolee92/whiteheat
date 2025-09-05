dd890116cc232442fc06efa37e7a73070f513a1a
Joo Lee <joo.lee08@qmail.cuny.edu>
2025-09-05 13:01:22 -0400

    Initial commit

---
+++ WhiteHeat/WhiteHeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhiteHeatApp.swift
+//  WhiteHeat
+//
+//  Created by Joo Lee on 9/5/25.
+//
+
+import SwiftUI
+
+@main
+struct WhiteHeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
