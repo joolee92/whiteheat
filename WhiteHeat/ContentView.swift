9ab930aa2c481895ef12ec6f57db443296049cf0
Joo Lee <joo.lee08@qmail.cuny.edu>
2025-09-05 16:07:20 -0400

    If pressing play after pressing pause, audio resumes

--- WhiteHeat/ContentView.swift
+++ WhiteHeat/ContentView.swift
@@ -10,27 +10,35 @@ import AVFoundation
 
 struct ContentView: View {
     @State private var player: AVAudioPlayer?
+    var url = Bundle.main.url(forResource: "swimming_in_the_dark", withExtension: "wav")
     var body: some View {
         HStack {
-            Button("Play"){
-                if let url = Bundle.main.url(forResource: "swimming_in_the_dark", withExtension: "wav") {
+            Button("Play") {
+                if let player = player {
+                    if !player.isPlaying {
+                        player.play()
+                    }
+                } else {
                     do {
-                        player = try AVAudioPlayer(contentsOf: url)
+                        player = try AVAudioPlayer(contentsOf: url!)
+                        player?.prepareToPlay()
                         player?.play()
                     } catch {
-                        print("Error playing audio")
+                        print("Error playing audio: \(error.localizedDescription)")
                     }
                 }
             }
-            Button("Pause"){
-                if ((player?.isPlaying) != nil) {
-                    player?.pause()
+            Button("Pause") {
+                if let player = player, player.isPlaying {
+                    player.pause()
                 }
             }
         }
         .buttonStyle(.bordered)
     }
 }
+
+
 
 #Preview {
     ContentView()
